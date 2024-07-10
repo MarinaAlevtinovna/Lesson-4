0283cf73c7c9bf6311797710f497e8aadaeb35a9
MarinaAlevtinovna <myssya@gmail.com>
2024-07-10 13:34:35 +0300

    add new class

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-a = 5
-b = 9
-c = 23
+class Warrior():
+    def __init__(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
+    def sleep(self):
+        print(f'{self.name} лег спать')
+        self.endurance += 2
+
+    def eat(self):
+        print(f'{self.name} сел кушать')
+        self.power += 1
+
+    def hit(self):
+        print(f'{self.name} бьет кого-то')
+        self.endurance -= 5
+
+    def walk(self):
+        print(f'{self.name} идет')
+
+    def info(self):
+        print(f'имя воина - {self.name}')
+        print(f'сила - {self.power}')
+        print(f'выносливость - {self.endurance}')
+        print(f'цвет волос - {self.hair_color}')
+        
